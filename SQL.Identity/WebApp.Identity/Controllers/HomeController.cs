92fe017965d544c06dca3643a7c4b9ea01e493cf
Snyp <gabrielmarques.mg@gmail.com>
2019-07-02 13:12:01 -0300

    [Add]_All

---
+++ SQL.Identity/WebApp.Identity/Controllers/HomeController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Diagnostics;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WebApp.Identity.Models;
+
+namespace WebApp.Identity.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly UserManager<MyUser> _userManager;
+        private readonly SignInManager<MyUser> _signInManager;
+        private readonly IUserClaimsPrincipalFactory<MyUser> _userClaimsPrincipalFactory;
+
+        public HomeController(UserManager<MyUser> userManager, SignInManager<MyUser> signInManager, IUserClaimsPrincipalFactory<MyUser> userClaimsPrincipalFactory)
+        {
+            this._userManager = userManager;
+            this._signInManager = signInManager;
+            this._userClaimsPrincipalFactory = userClaimsPrincipalFactory;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(Login model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Salva o estado de login no cookie (manter logado após fechar browser) [true = manter | false = não manter]
+                var persistLogin = false;
+
+                // Bloquear a conta do usuário após tentativas sem sucesso de logar [true = bloquear | false = não bloquear]
+                var lockUser = false;
+
+                var signInResult = await _signInManager.PasswordSignInAsync(model.UserName, model.Password, persistLogin, lockUser);
+
+                if (signInResult.Succeeded)
+                {
+                    return RedirectToAction("About");
+                }
+
+                ModelState.AddModelError("", "Usuário ou senha inválido!");
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(Register model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByNameAsync(model.UserName);
+
+                if (user == null)
+                {
+                    user = new MyUser()
+                    {
+                        Id = Guid.NewGuid().ToString(),
+                        UserName = model.UserName
+                    };
+
+                    var result = await _userManager.CreateAsync(user, model.Password);
+                }
+
+                return View("Success");
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult ForgotPassword()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ForgotPassword(ForgotPassword model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByEmailAsync(model.Email);
+
+                if(user != null)
+                {
+                    var token = await _userManager.GeneratePasswordResetTokenAsync(user);
+                    var resetUrl = Url.Action("ResetPassword", "Home", 
+                        new {
+                            token = token,
+                            email = model.Email
+                       }, Request.Scheme);
+
+                    System.IO.File.WriteAllText("resetLink.txt", resetUrl);
+                }
+            }
+        }
+
+        [HttpGet]
+        public IActionResult ResetPassword()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ResetPassword(ResetPassword model)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
+        }
+
+        [HttpGet]
+        [Authorize]
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Success()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
